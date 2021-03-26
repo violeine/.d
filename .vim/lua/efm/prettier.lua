d98586de0458e90a65501452fa1846ab404927a0
violeine <violeaol@gmail.com>
2021-03-26 14:21:46 +0000

    efm

---
+++ .vim/lua/efm/prettier.lua
@@ -0,0 +1,12 @@
+return {
+    formatCommand = ([[
+        $HOME/.node_modules/bin/prettier
+        ${--config-precedence:configPrecedence}
+        ${--tab-width:tabWidth}
+        ${--single-quote:singleQuote}
+        ${--trailing-comma:trailingComma}
+    ]]):gsub(
+        "\n",
+        ""
+    )
+}
