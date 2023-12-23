68f7c1d2bdd0a3c0c82cd60d8e585f3140155199
violeine <haluudang@gmail.com>
2023-12-23 21:45:04 +0700

    fix symlink path for nvim

--- flake.nix
+++ flake.nix
@@ -30,12 +30,12 @@
           # Optionally use extraSpecialArgs
           # to pass through arguments to home.nix
         };
-      };
         "violeine@deck" = home-manager.lib.homeManagerConfiguration {
 
           # Specify your home configuration modules here, for example,
           # the path to your home.nix.
-          modules = [ ./home.nix {
+          modules = [ ./home.nix 
+          {
             home.username = "violeine";
             home.homeDirectory = "/var/home/violeine";
           }
@@ -46,4 +46,5 @@
           # to pass through arguments to home.nix
         };
       };
+    };
     }
--- nvim/default.nix
+++ nvim/default.nix
@@ -1,9 +1,9 @@
-{pkgs,...}:{
+{pkgs, config, ...}:{
   home.packages = with pkgs; [
     neovim 
   ]; 
   home.file.".config/nvim" = {
-    source = ./.;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.d/nvim";
     recursive = true;
   };
 }
